ce38852e7bee6185957bdf893b3ca0e2745e2544
Daniel Deng <denjm@foxmail.com>
2023-04-13 14:10:41 +0100

    update

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2');
 
 const poll = mysql.createPool({
-    host: 'localhost',
+    host: '3.248.182.99',
     port: 3306,
     user: 'root',
     password: 'dengjiaming123',
